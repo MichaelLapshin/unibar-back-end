d9185bf3658384b2451413518c000668b03d026e
Michael Lapshin <mrmichael.lapshin@gmail.com>
2023-10-11 18:28:34 -0400

    Reordered db delete script

--- database/delete_database.sql
+++ database/delete_database.sql
@@ -1,7 +1,7 @@
+DROP TABLE `UniBar`.`Report`;
+DROP TABLE `UniBar`.`Order`;
+DROP TABLE `UniBar`.`Contact`;
 DROP TABLE `UniBar`.`Users`;
 DROP TABLE `UniBar`.`Admins`;
-DROP TABLE `UniBar`.`Order`;
-DROP TABLE `UniBar`.`Report`;
-DROP TABLE `UniBar`.`Contact`;
 
 DROP DATABASE `UniBar`;
