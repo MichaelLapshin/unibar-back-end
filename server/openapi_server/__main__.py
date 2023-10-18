6673a2518da5c99970d6a0db291e877d38bfb380
Michael Lapshin <mrmichael.lapshin@gmail.com>
2023-10-18 11:17:57 -0400

    Display process pid

--- server/openapi_server/__main__.py
+++ server/openapi_server/__main__.py
@@ -2,11 +2,17 @@
 
 import connexion
 import datetime
+import os
 
 from openapi_server import encoder
 from openapi_server import server_attr
 
 def main():
+    # Save the PID into a file
+    print("PID:", os.getpid())
+    with open('latest_deployment_pid.txt', 'w') as f:
+        f.write(os.getpid())
+
     # Launch the server
     server_attr.start_time = datetime.datetime.now()
     app = connexion.App(__name__, specification_dir='./openapi/')
